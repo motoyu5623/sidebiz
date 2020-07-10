5f83fe89600f2065f6cfa2062be7fadd0b9f647b
坂本悠輔 <sakamotoyuusuke@mbp.local>
2020-07-10 16:11:39 +0900

    style: to pass rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validate :username, presence: true, length: { maximum: 50 }
-  validate :profile, length: { maximum: 300 }
 
+  validates :username, presence: true, length: { maximum: 50 }
+  validates :profile, length: { maximum: 300 }
 end
