ff4848628114dc08b0b275e0b0ec6efd92408217
坂本悠輔 <sakamotoyuusuke@mbp.local>
2020-07-11 16:47:47 +0900

    test: add user model spec mainly validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user)}
+  let(:other_user) { create(:user)}
+  
+  it "is valid with username, email, birtyday, password" do
+    expect(user).to be_valid
+  end
+
+  it "is invalid without username" do
+    user.username = nil
+    user.valid?
+    expect(user.errors[:username]).to include("can't be blank")
+  end
+
+  it "is invalid without email" do
+    user.email = nil
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it "is invalid without birtyday" do
+    user.birthday = nil
+    user.valid?
+    expect(user.errors[:birthday]).to include("can't be blank")
+  end
+
+  # it "is invalid without password" do
+  #   user.password = nil
+  #   user.valid?
+  #   expect(user.errors[:password]).to include("can't be blank")
+  # end
+  it "is invalid with a duplicate email address" do
+    other_user.email = user.email 
+    other_user.valid?
+    expect(other_user.errors[:email]).to include('has already been taken')
+  end
 end
--- spec/requests/staticpage_request_spec.rb
+++ spec/requests/staticpage_request_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe 'Testpages', type: :request do
+RSpec.describe 'Staticpages', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/testpage/index'
+      get '/staticpage/index'
       expect(response).to have_http_status(:success)
     end
   end
