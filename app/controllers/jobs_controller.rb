86bd90c8998c4ef6ff2782b1dbb9a830ef2c3bed
坂本悠輔 <sakamotoyuusuke@mbp.local>
2020-07-12 17:59:09 +0900

    feat: add job new & show pages

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,6 @@
 class JobsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @jobs = Job.all
   end
@@ -8,11 +10,19 @@ class JobsController < ApplicationController
   end
 
   def new
-    @job = Job.new
+    @job = current_user.jobs.build
   end
 
   def create
-    Job.create(job_params)
+    @job = current_user.jobs.build(job_params)
+    @job.save
+    if @job.save
+      flash[:success] = "job registered!"
+      redirect_to job_path(@job.id)
+    else
+      flash[:success] = "register faild"
+      render :new
+    end
   end
 
   def edit
@@ -30,9 +40,10 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:name, :type, :section, :industry,
+    params.require(:job).permit(:name, :work_type, :company, :section,
+                                :industry,
                                 :medium, :occupation, :started_at,
                                 :ended_at, :worktime_week, :description,
-                                :pulled_skill, :returned_skill, :is_main)
+                                :pulled_skill, :returned_skill, :is_main, :user_id)
   end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,15 +1,17 @@
 class Job < ApplicationRecord
   belongs_to :user
 
+  # validates :user_id, presence: true
+
   validates :name, presence: true, length: { maximum: 50 }
-  validates :work_type, length: { maximum: 50 }
-  validates :section, length: { maximum: 50 }
-  validates :industry, length: { maximum: 50 }
-  validates :medium, length: { maximum: 50 }
-  validates :occupation, length: { maximum: 50 }
+  # validates :work_type, length: { maximum: 50 }
+  # validates :section, length: { maximum: 50 }
+  # validates :industry, length: { maximum: 50 }
+  # validates :medium, length: { maximum: 50 }
+  # validates :occupation, length: { maximum: 50 }
   validates :worktime_week, presence: true
-  validates :description, presence: true, length: { maximum: 400 }
-  validates :pulled_skill, presence: true, length: { maximum: 400 }
-  validates :returned_skill, presence: true, length: { maximum: 400 }
-  validates :is_main, presence: true
+  # validates :description, presence: true, length: { maximum: 400 }
+  # validates :pulled_skill, presence: true, length: { maximum: 400 }
+  # validates :returned_skill, presence: true, length: { maximum: 400 }
+  # validates :is_main, presence: true
 end
