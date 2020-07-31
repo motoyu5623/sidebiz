85d58dc768c53a957c524cedc6bd4e4fe1331f5a
motoyu5623 <motoyu5623@gmail.com>
2020-07-31 14:54:46 +0900

    feat: image upload to s3

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'devise'
 gem 'font_awesome5_rails'
 gem 'kaminari'
 gem 'ransack'
+gem 'aws-sdk-s3', require: false
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -63,7 +63,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[username birthday profile living_place])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[username birthday profile living_place avatar])
   end
 
   # The path used after sign up.
