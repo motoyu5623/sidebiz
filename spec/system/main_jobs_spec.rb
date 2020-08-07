14fff45e7760179a0df97cc09f2dc5ca7dec5404
motoyu5623 <motoyu5623@gmail.com>
2020-08-07 17:03:16 +0900

    build: introduce chromedriver for system spec

---
+++ spec/system/main_jobs_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'MainJobs', type: :system do
+  let!(:user) { create(:user) }
+  # let!(:main_job) { create(:main_job, user: user) }
+
+  scenario "a user register main job" do
+    login_as(user)
+    visit new_main_job_path
+    expect(current_path).to eq new_main_job_path
+    expect(page).to have_content ""
+    fill_in 'main_job_name', with: "WEBエンジニア"
+    click_button "登録する"
+    expect(current_path).to eq user_path(user)
+    expect(page).to have_content "#{user.username}のページ"
+    expect(page).to have_content "WEBエンジニア"
+    expect(page).to have_content "本業の追加登録"
+    expect(page).to have_content "アカウント編集"
+  end
+
+end
