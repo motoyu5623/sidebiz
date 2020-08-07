1d1c98670b3a6526a4db3189e55c0110d74dfea1
motoyu5623 <motoyu5623@gmail.com>
2020-08-07 18:43:51 +0900

    test: system spec main_job

--- spec/system/main_jobs_spec.rb
+++ spec/system/main_jobs_spec.rb
@@ -19,6 +19,21 @@ RSpec.describe 'MainJobs', type: :system do
       expect(page).to have_content "本業の追加登録"
       expect(page).to have_content "アカウント編集"
     }.to change(user.main_jobs, :count).by(1)
+
+    
+    expect {
+      visit edit_main_job_path(user.main_jobs.last)
+      expect(current_path).to eq edit_main_job_path(user.main_jobs.last)
+      select '20〜30万円', from: "main_job_income_month"
+      click_button "更新する"
+      expect(current_path).to eq user_path(user)
+      expect(page).to have_content "編集に成功しました"
+    }.to_not change(user.main_jobs, :count)
+
+    expect {
+      visit edit_main_job_path(user.main_jobs.last)
+      click_link "削除"
+    }.to change(user.main_jobs, :count).by(-1)
   end
 
 end
