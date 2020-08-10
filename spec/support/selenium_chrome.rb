6ace40fcd03544ba848404038d5c535280a861d8
motoyu5623 <motoyu5623@gmail.com>
2020-08-10 13:27:53 +0900

    text: fix need server? error

--- spec/support/selenium_chrome.rb
+++ spec/support/selenium_chrome.rb
@@ -1,7 +1,7 @@
 require 'capybara/rspec'
 require 'selenium-webdriver'
 
-Capybara.register_driver :selenium_chrome_headless do
+Capybara.register_driver :selenium_chrome_headless do |app|
   options = ::Selenium::WebDriver::Chrome::Options.new
 
   options.add_argument('--headless')
@@ -9,7 +9,7 @@ Capybara.register_driver :selenium_chrome_headless do
   options.add_argument('--disable-dev-shm-usage')
   options.add_argument('--window-size=1400,1400')
 
-  # driver = Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
 Capybara.javascript_driver = :selenium_chrome_headless
